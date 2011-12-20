336d7ca67e044215f77563d9610b92e1c647a65d
Olivier Gorzalka <olivier@clearideaz.com>
2011-12-20 17:25:32 +0100

    missing double equal ;)

--- kerning.js
+++ kerning.js
@@ -222,7 +222,7 @@
                 if($.parsecss.mediumApplies(split.join(' '))) {
                     $.parsecss(css, callback);
                 }
-            } else if (type='import') {
+            } else if (type=='import') {
                 var url = restore(split.shift());
                 if($.parsecss.mediumApplies(split.join(' '))) {
                     url = url.replace(/^url\(|\)$/gi, '').replace(/^["']|["']$/g, ''); // remove the url('...') wrapper
